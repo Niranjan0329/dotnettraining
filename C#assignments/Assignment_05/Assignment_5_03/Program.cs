355e244c1f5b4e13f1f469d83cf6532513351c9d
Niranjan0329 <114423587+Niranjan0329@users.noreply.github.com>
2022-10-10 22:43:14 +0530

    assignmet

---
+++ C#assignments/Assignment_05/Assignment_5_03/Program.cs
@@ -0,0 +1,48 @@
+﻿
+/*3.
+ I.Create a class library Project called Concession. Write a method
+called CalculateConcession(int age) 
+that takes age as an input and calculates concession for travel as below:
+If age<=5 then “Little Champs- Free Ticket” should be displayed
+If age >60 then calculate 30% concession on the totalfare(Which is a
+constant Eg:500 / -) and Display “ Senior Citizen” + Calculated Fare
+Else “Print Ticket Booked” + Fare. 
+
+II. Create a Console application with a Class called Program which 
+has int TotalFare as Constant, Name, Age.  Accept Name, Age from the user and 
+call the CalculateConcession() function
+(Hint: Add required references)*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concession;
+
+namespace ConsoleApp123
+{
+    
+    class Program
+    {
+    
+    public  static void Main(string[] args)
+        {
+            
+
+            classlibrary cl = new classlibrary();
+            double totalfare = 500;
+            int Age;
+
+            Console.WriteLine("enter the name");
+          string name = Console.ReadLine();
+            Console.WriteLine("enter the age");
+            cl.Age= Convert.ToInt32(Console.ReadLine());
+            cl.CalculateConcession(cl.Age);
+            Console.Read();
+            
+
+            
+        }
+    }
+}
---
+++ C#assignments/Assignment_05/Assignment_5_03/classlibrary.cs
@@ -0,0 +1,45 @@
+﻿
+/*3.
+ I.Create a class library Project called Concession. Write a method
+called CalculateConcession(int age) 
+that takes age as an input and calculates concession for travel as below:
+If age<=5 then “Little Champs- Free Ticket” should be displayed
+If age >60 then calculate 30% concession on the totalfare(Which is a
+constant Eg:500 / -) and Display “ Senior Citizen” + Calculated Fare
+Else “Print Ticket Booked” + Fare*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession
+{
+    public class classlibrary
+    {
+        public int Age;
+        public void CalculateConcession(int age)
+            
+        {
+
+            double concession ;
+            double totalfare = 500;
+      
+            if(age <= 5)
+            {
+                Console.WriteLine("“Little Champs- Free Ticket”");
+            }
+      else if(age>60) 
+            {
+                concession = totalfare * 0.3;
+                double totalfare2 = totalfare - concession;
+                Console.WriteLine("senior citizen {0}", totalfare2);
+            }
+            else
+            {
+                Console.WriteLine("print ticket booked {0}" ,totalfare);
+            }
+        }
+    }
+}
