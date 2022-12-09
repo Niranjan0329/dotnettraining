5f187198ba8ce50e6c0f3a4d342420125d5e7bfe
Niranjan0329 <114423587+Niranjan0329@users.noreply.github.com>
2022-12-09 12:51:31 +0530

    Final project

---
+++ C#assignments/Final project/applicationapi/applicationapi/applicationapi/Controllers/ManagerRepotController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using applicationapi.Models;
+
+namespace applicationapi.Controllers
+{
+    public class ManagerRepotController : ApiController
+    {
+        newtravelbookingsystemEntities db = new newtravelbookingsystemEntities();
+        public IHttpActionResult getEditing()
+        {
+            var customer = from cust in db.TravelRequests
+                           where cust.CurrentStatus == "TicketConformed" || cust.CurrentStatus == "TicketnotConformed" || cust.CurrentStatus == "Pending" || cust.CurrentStatus == "Approved" || cust.CurrentStatus == "Rejected"
+                           select cust;
+
+            return Ok(customer.ToList());
+            //    IEnumerable<employeelist_Result> result = db.employeelist();
+            //    return Ok(result);
+        }
+    }
+}
---
+++ C#assignments/Final project/applicationapi/applicationapi/applicationmvc/Models/Managermodel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace applicationmvc.Models
+{
+    public class Managermodel
+    {
+        public int Requestid { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        public Nullable<System.DateTime> RequestDate { get; set; }
+        public string FromLocation { get; set; }
+        public string ToLocation { get; set; }
+        public Nullable<int> UserId { get; set; }
+        public string CurrentStatus { get; set; }
+    }
+
+   
+}
